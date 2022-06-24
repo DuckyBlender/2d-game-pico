b048c31093c6807773d439f5632ed38a00aba024
DuckyBlender <alan@alanklas.com>
2022-06-24 13:41:04 +0200

    first commit, probably will rewrite soon

---
+++ game.py
@@ -0,0 +1,84 @@
+from machine import Pin, I2C, ADC, PWM
+from ssd1306 import SSD1306_I2C
+import json, utime
+
+# I2C connection to the display
+i2c = I2C(0,sda=Pin(0), scl=Pin(1), freq=400000)
+
+# Setup joystick
+xAxis = ADC(Pin(26))
+yAxis = ADC(Pin(27))
+button = Pin(16, Pin.IN, Pin.PULL_UP)
+
+# Setup display
+oled = SSD1306_I2C(128, 64, i2c)
+oled.contrast(255)
+oled.fill(0)
+
+def translate(value, leftMin, leftMax, rightMin, rightMax):
+    # Figure out how 'wide' each range is
+    leftSpan = leftMax - leftMin
+    rightSpan = rightMax - rightMin
+
+    # Convert the left range into a 0-1 range (float)
+    valueScaled = float(value - leftMin) / float(leftSpan)
+
+    # Convert the 0-1 range into a value in the right range.
+    return rightMin + (valueScaled * rightSpan)
+
+blockSize = 4
+
+map = [[1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 2, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]]
+
+xOffset, yOffset = 0, 0
+duration = 69420
+
+while True:
+    oled.fill(0)
+
+    start = utime.ticks_ms()
+    
+    xValue = int(translate(xAxis.read_u16(), 800, 60000, -1, 1)) * -1
+    yValue = int(translate(yAxis.read_u16(), 800, 60000, -1, 1))
+    print(f"xValue: {xValue} yValue: {yValue} raw: {xAxis.read_u16()} {yAxis.read_u16()}")
+    buttonValue = button.value()
+    
+    
+    
+    xOffset += xValue * -1
+    yOffset += yValue * -1
+    
+
+    # Invert value
+#    print(f"x: {xValue} y: {yValue} button: {buttonValue}")
+    for x in range(0,32):
+        for y in range(0,14):
+            #print(f"{x}:{y} = {map[y][x]}")
+    
+            if map[y][x] == 1:
+                oled.fill_rect((x+xOffset)*blockSize-1+64, (y+yOffset)*blockSize-1+32, blockSize, blockSize, 1)
+
+    oled.rect(63, 31, 2, 2, 1)
+    
+    if buttonValue == 0:
+        xOffset, yOffset = 0, 0
+    
+    if duration == 69420: # If first frame, skip calculating FPS
+        pass
+    oled.text(f"fps:{1000/duration:.0f}X:{xOffset}Y:{yOffset}", 0, 56, 1)
+    #time.sleep(0.1)
+    oled.show()
+    duration = utime.ticks_ms() - start
